9b901dd86fa6f5ee1fb70338374949905ef30781
Vitor Bigelli <vitor@nindoo.ai>
2021-04-12 15:25:52 -0300

    feat: change .env requirement method

--- src/common/Api.js
+++ src/common/Api.js
@@ -1,12 +1,14 @@
 'use strict'
 import axios from 'axios'
-import { AUTENTIQUE_TOKEN } from 'babel-dotenv'
+import { AUTENTIQUE_TOKEN } from 'babel-dotenv' 
+
+require('dotenv').config()
 
 const Api = (token) => axios.create({
   baseURL: 'https://api.autentique.com.br/v2',
   timeout: 1000,
   headers: {
-    Authorization: `Bearer ${token || AUTENTIQUE_TOKEN}`
+    Authorization: `Bearer ${token || process.env.AUTENTIQUE_TOKEN}`
   }
 })
 
